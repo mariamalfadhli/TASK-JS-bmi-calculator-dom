56567335e96cb6f0f1dbae0a637a9407dc937585
Mariam <Maf9894@gmail.com>
2023-05-09 00:08:07 +0300

    done

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ function calculateBMI() {
   let male = document.getElementById("m").checked;
   let female = document.getElementById("f").checked;
   console.table({ age, weight, height, male, female });
-  let BMI = (100* weight/(height^2));
-  alert(BMI)
+  height= height/100
+  let BMI = (weight/(height*height));
+  alert("your BMI:" + BMI)
 
   // You are going to write the code here before the curly braces 👇🏻
 
   if (BMI <= 18.5){ alert("you are underweight")}
-  else if (BMI >= 18.5 || BMI <= 24.9) {alert("your healthy weight")}
-  else alert("you are overweight")
+  else if (BMI >= 18.5 && BMI <= 24.9) {alert("you are healthy ")}
+  else if(BMI >= 25 && BMI <= 29.9) alert("you are overweight")
+else alert("you are obesite")
 
+if ((age >=19 && age <=24 ) && (BMI >= 19 && BMI <= 24) ) {alert ("healthy")}
+else if ((age >=25 && age <=34 ) && (BMI >= 20 && BMI <= 25) ) {alert ("healthy")}
+else if ((age >=35 && age <=44 ) && (BMI >= 21 && BMI <= 26) ) {alert ("healthy")}
+else if ((age >=45 && age <=54 ) && (BMI >= 22 && BMI <= 27) ) {alert ("healthy")}
+else if ((age >=55 && age <=64 ) && (BMI >= 23 && BMI <= 28) ) {alert ("healthy")}
+else if ((age >= 65 ) && (BMI >= 24 && BMI <= 29) ) {alert ("healthy")}
+else alert("unhealthy")
 }
